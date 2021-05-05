b2fe930dbeb57eb77af693050de9c4e70a45ab27
lauradillonar <75162401+lauradillonar@users.noreply.github.com>
2021-05-05 09:53:40 -0300

    Delete curso-flask-u20-galeria-imagenes directory

--- curso-flask-u20-galeria-imagenes/aplicacion/forms.py
+++
@@ -1,9 +0,0 @@
-
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from flask_wtf.file import FileField, FileRequired
-
-
-class UploadForm(FlaskForm):
-    photo = FileField('selecciona imagen:', validators=[FileRequired()])
-    submit = SubmitField('Submit')
