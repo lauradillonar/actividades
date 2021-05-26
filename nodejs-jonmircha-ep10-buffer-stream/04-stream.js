8e4947e5deec725e67f34ca19634096354e3fb0a
lauradillonar <75162401+lauradillonar@users.noreply.github.com>
2021-05-26 10:30:46 -0300

    Add files via upload

---
+++ nodejs-jonmircha-ep10-buffer-stream/04-stream.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var fs=require('fs'),
+	readStream=fs.createReadStream('assets/nombres.txt'),
+	writeStream=fs.createWriteStream('assets/nombres_copia.txt')
+
+	readStream.pipe(writeStream)
+
+	readStream.on('data', function(chunk){
+		console.log('He leido:',chunk.length,'caracteres')
+	}) 
+
+	readStream.on('end',function(){
+		console.log('Terminé de leer el archivo')
+	})
