49d514c5ffa4c5f818d3fe6737f9a00ead5d0be3
lauradillonar <75162401+lauradillonar@users.noreply.github.com>
2021-05-31 19:19:34 -0300

    Add files via upload

---
+++ algoritmos-john-ortiz-ordoñez/extra001_multiplicacion.py
@@ -0,0 +1,23 @@
+# Fuente: https://www.youtube.com/watch?v=MXmQM_Uehtk
+# Actividad 1: Como multiplicar dos números naturales sin utilizar el signo de multiplicación
+
+def multiply (a, b):
+    result=0
+    if abs(a) == a:
+        positive_a = True
+    else:
+        positive_a = False
+
+    for i in range(abs(a)):
+        if positive_a:
+            result+=b
+        else:
+            result-=b 
+    
+    return result
+
+num1 = -5
+num2 = -2
+print('{} por {} es {}'.format(num1,num2, multiply(num1,num2)))
+
+
