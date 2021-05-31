ef9c0b2cec3ebf77af39df9ff53775d97141a180
lauradillonar <75162401+lauradillonar@users.noreply.github.com>
2021-05-31 08:54:40 -0300

    Add files via upload

---
+++ algoritmos-john-ortiz-ordoñez/e0028_diferencia_conjuntos.py
@@ -0,0 +1,15 @@
+# Ejercicio 28: Calcular la diferencia de conjuntos con colores.
+
+colores_lista_1 = ['Negro', 'Rojo', 'Verde', 'Blanco']
+colores_lista_2 = ['Blanco', 'Azul', 'Verde', 'Gris', 'Amarillo', 'Verde']
+
+# A, B; A - B; A / B
+
+colores_conjunto_1 = set(colores_lista_1)
+colores_conjunto_2 = set(colores_lista_2)
+
+diferencia = colores_conjunto_2.difference(colores_conjunto_1)
+
+print('B-A=',diferencia)
+print('A=',colores_conjunto_1)
+print('B=',colores_conjunto_2)
